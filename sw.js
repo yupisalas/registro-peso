4d03d69b67ffd5bea925816d427db39aa68e7b7e
yupisalas <santiago.garcia@sequra.es>
2026-08-23 22:43:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service worker: cachea la app para que funcione offline.
-const CACHE = "registro-peso-v17";
+// Service worker: network-first, con activación inmediata de versiones nuevas.
+const CACHE = "registro-peso-v18";
 const ASSETS = ["./", "./index.html", "./bundle.js", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
@@ -11,9 +11,8 @@ self.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
